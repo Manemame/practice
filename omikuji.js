0b1110d781ea4235f6676a4a08fea7ad2aba87ad
Manemame <149764350+Manemame@users.noreply.github.com>
2023-11-07 15:53:12 +0900

    Add files via upload

--- omikuji.js
+++ omikuji.js
@@ -4,7 +4,7 @@ let userresult;
 
 username = prompt("お名前を教えて下さい。");
 document.getElementById("name").innerHTML = username;
-document.getElementById8("").innerHTML=名無し
+document.getElementById8("").innerHTML=名無し;
 
 let rand = Math.floor( Math.random() * 5);
 if (rand == 0) {
